35163738119347b5f4279f7eb3b754e1bbb8fbf2
ahah525 <ahah525@naver.com>
2022-08-02 10:42:19 +0900

    @Controller, @GetMapping 어노테이션 구현

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -9,10 +9,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/usr/*")
-public class HelloServlet extends HttpServlet {
-
+public class DispatchServlet extends HttpServlet {
+    // 조회
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("Hi");
+    }
+
+    // 등록
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    // 수정
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    // 삭제
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
     }
 }
--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -1,4 +1,15 @@
 package com.ll.exam.article.controller;
 
+import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.GetMapping;
+
+// ArticleController == controller
+@Controller
 public class ArticleController {
+
+    // /usr/article/list GET 요청 처리
+    @GetMapping("/usr/article/list")
+    public void showList() {
+
+    }
 }
