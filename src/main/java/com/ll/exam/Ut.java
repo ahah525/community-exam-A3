8957c68e41bda1666e6acdf5529286d69a974998
ahah525 <ahah525@naver.com>
2022-08-02 13:37:16 +0900

    newObj 제너릭 적용하여 형변환 생략

--- src/main/java/com/ll/exam/Ut.java
+++ src/main/java/com/ll/exam/Ut.java
@@ -4,9 +4,10 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Ut {
     public static class cls {
-        // 해당 클래스 타입의 객체 생성(Object 버전)
-        public static Object newObj(Class cls, Object defaultValue) {
+        // 해당 클래스 타입의 객체 생성(Generic 버전)
+        public static <T> T newObj(Class<T> cls, T defaultValue) {
             try {
+                // 객체 생성
                 return cls.getDeclaredConstructor().newInstance();
             } catch (InstantiationException e) {
                 return defaultValue;
