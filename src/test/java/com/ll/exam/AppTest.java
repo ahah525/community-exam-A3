519115844a1f642b4afb488f993f7d6dc69fc23f
ahah525 <ahah525@naver.com>
2022-08-02 10:31:18 +0900

    Controller를 통해 ArticleController를 싱글톤 관리 구현 및 검증

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,5 +11,21 @@ public class AppTest {
         int rs = 10 + 20;
 
         assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    public void ioc__articleController() {
+        // when: con에서 ArticleController 받아옴
+        ArticleController articleController = Con.getArticleController();
+        // then: 받아온 articleController가 null이 아닌지 검증
+        assertThat(articleController).isNotNull();
+    }
+
+    @Test
+    public void ioc__articleController__2() {
+        ArticleController articleController1 = Con.getArticleController();
+        ArticleController articleController2 = Con.getArticleController();
+        // then: 같은 객체가 반환되는지 검증(싱글톤 검증)
+        assertThat(articleController1).isEqualTo(articleController2);
     }
 }
